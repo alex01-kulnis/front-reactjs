21fb523b0ad4bba797c0bfcf0209feda46f7a1f9
alexander <kulnis71@gmail.com>
2022-10-02 19:07:01 +0300

    init, start files

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElem = document.getElementById('root');
+
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+
+  root.render(
+    // <BrowserRouter>
+    //   <Provider store={store}>
+    <App />,
+    //   </Provider>
+    // </BrowserRouter>,
+  );
+}
