3713c1388f4a589a3b9ab52398c2b1490f768fb8
alexander <kulnis71@gmail.com>
2023-03-28 23:11:45 +0300

    add styles header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import robotSvg from '../../assets/robot-logo.svg';
+import personSvg from '../../assets/person.svg';
 
 import './header.scss';
 
 const Header: React.FC = () => {
-  return <header className="header">Header</header>;
+  return (
+    <header className="header">
+      <div className="container">
+        <Link to="/">
+          <div className="header__logo">
+            <img width="36" src={robotSvg} alt="logo" />
+            <div>
+              <h1>Hobby Street</h1>
+            </div>
+          </div>
+        </Link>
+        <div className="header__nav">
+          <div className="header__inscription">Главная</div>
+          <div className="header__inscription">Заявки</div>
+          <div className="header__inscription">Секции</div>
+          <div className="header_person">
+            <img width="36" src={personSvg} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
