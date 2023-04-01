13d8f41d8c1986da6c4fa6a46725369dfc4a42d6
alexander <kulnis71@gmail.com>
2023-04-01 23:09:21 +0300

    fix org-form(2)

--- src/components/oraganization-form/OrganizationForm.tsx
+++ src/components/oraganization-form/OrganizationForm.tsx
@@ -9,70 +9,52 @@ type OrganizationFormProps = {
 
 const OrganizationForm: React.FC<OrganizationFormProps> = ({ value }) => {
   return (
-    <div className="block-org-form">
-      <div className={value === 2 ? 'active-content-block-org-form' : 'content-block-org-form'}>
-        <div className="block-input">
+    <div className={value === 2 ? 'active-content-block-org-form' : 'content-block-org-form'}>
+      <div className="block-org-input">
+        <div className="block-content">
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация</span>
             </label>
           </div>
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация1</span>
             </label>
           </div>
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
-            </label>
-          </div>
-        </div>
-        <div className="block-input">
-          <div className="input-field">
-            <input type="text" name="name" autoComplete="off" required />
-            <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
-            </label>
-          </div>
-          <div className="input-field">
-            <input type="text" name="name" autoComplete="off" required />
-            <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
-            </label>
-          </div>
-          <div className="input-field">
-            <input type="text" name="name" autoComplete="off" required />
-            <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация2</span>
             </label>
           </div>
         </div>
-        <div className="block-input">
+        <div className="block-content">
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация</span>
             </label>
           </div>
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация1</span>
             </label>
           </div>
           <div className="input-field">
             <input type="text" name="name" autoComplete="off" required />
             <label htmlFor="Name" className="label-name">
-              <span className="content-name">Name</span>
+              <span className="content-name">организация2</span>
             </label>
           </div>
+        </div>
+        <div className="sumbit-button">
+          <button>Регистрация</button>
         </div>
       </div>
-      <button type="submit">Регистрация</button>
     </div>
   );
 };
--- src/components/user-form/UserForm.tsx
+++ src/components/user-form/UserForm.tsx
@@ -8,19 +8,30 @@ type UserFormProps = {
 
 const UserForm: React.FC<UserFormProps> = ({ value }) => {
   return (
-    <div className={value === 1 ? 'content  active-content' : 'content'}>
-      <div className="field-item">
-        <label htmlFor="">Логин</label>
-        <input type="text" />
+    <div className={value === 1 ? 'active-content-block-user-form' : 'content-block-user-form'}>
+      <div className="block-user-form">
+        <div className="block-input">
+          <div className="input-field">
+            <input type="text" name="name" autoComplete="off" required />
+            <label htmlFor="Name" className="label-name">
+              <span className="content-name">Name</span>
+            </label>
+          </div>
+          <div className="input-field">
+            <input type="text" name="name" autoComplete="off" required />
+            <label htmlFor="Name" className="label-name">
+              <span className="content-name">Name</span>
+            </label>
+          </div>
+          <div className="input-field">
+            <input type="text" name="name" autoComplete="off" required />
+            <label htmlFor="Name" className="label-name">
+              <span className="content-name">Name</span>
+            </label>
+          </div>
+        </div>
       </div>
-      <div className="field-item">
-        <label htmlFor="">Пароль</label>
-        <input type="text" />
-      </div>
-      <div className="field-item">
-        <label htmlFor="">Повторите пароль</label>
-        <input type="text" />
-      </div>
+      <button type="submit">Регистрация</button>
     </div>
   );
 };
