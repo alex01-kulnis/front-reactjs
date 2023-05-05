2c4b0949825d4d1c69b32c7a3554065469e19f93
alexander <kulnis71@gmail.com>
2023-05-05 22:33:49 +0300

    add category-search

--- src/pages/general-pages/GeneralSectionPage.jsx
+++ src/pages/general-pages/GeneralSectionPage.jsx
@@ -4,10 +4,15 @@ import axios from "axios";
 import { observer } from "mobx-react-lite";
 
 import { SectionItem } from "../../components";
+import { useFieldState } from "../../hooks/useFieldState";
 
 import "./GeneralSectionPage.scss";
 
 const GeneralSectionPage = observer(() => {
+  const [category, setCategory] = useFieldState("Название секции");
+  const onCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
   const [sections, setSections] = useState([]);
   const [value, setValue] = useState("");
 
@@ -21,12 +26,23 @@ const GeneralSectionPage = observer(() => {
   };
 
   useEffect(() => {
-    // getUsers();
     getSections().then((data) => setSections(data));
   }, []);
 
   const filteredSections = sections.filter((section) => {
-    return section.name.toLowerCase().includes(value.toLocaleLowerCase());
+    if (category === "Название секции") {
+      return section.name.toLowerCase().includes(value.toLocaleLowerCase());
+    }
+
+    if (category === "Название организации") {
+      return section.user.organization_name
+        .toLowerCase()
+        .includes(value.toLocaleLowerCase());
+    }
+
+    if (category === "Имя преподователя") {
+      return section.mentor.toLowerCase().includes(value.toLocaleLowerCase());
+    }
   });
 
   return (
@@ -34,7 +50,13 @@ const GeneralSectionPage = observer(() => {
       <h1>Кружки и секции</h1>
       <div>
         <input onChange={(event) => setValue(event.target.value)} placeholder="Поиск..." />
+        <select onChange={onCategoryChange} value={category}>
+          <option>Название секции</option>
+          <option>Название организации</option>
+          <option>Имя преподователя</option>
+        </select>
       </div>
+
       <div className="filter">filter</div>
       <div className="main-sections">
         {filteredSections.map((sections) => (
